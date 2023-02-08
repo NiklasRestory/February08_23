bd720cbf9d2035dd6deddfe3dbbeee20d15b5d9f
NiklasRestory <niklas.cullberg@restory.ai>
2023-02-08 14:42:09 +0100

    First Commit

---
+++ February08_2023/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace February08_2023
+{
+    internal class Person
+    {
+    }
+}
---
+++ February08_2023/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace February08_2023
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
